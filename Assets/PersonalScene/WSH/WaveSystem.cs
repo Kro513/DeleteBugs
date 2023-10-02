cb98ae50cfb3e7e1c46754e7f887d09962dbb1fe
MAXracoon <143587600+MAXracoon@users.noreply.github.com>
2023-10-02 20:58:41 +0900

    aa

---
+++ Assets/PersonalScene/WSH/WaveSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Wave[] waves;
+    [SerializeField]
+    private EnemySpawner enemySpawner;
+    private int currentwaveIndex = -1;
+
+    public void StartWave()
+    {
+        if(enemySpawner.EnemyList.Count == 0 && currentwaveIndex <waves.Length-1)
+        {
+            currentwaveIndex ++;
+            enemySpawner.StartWave(waves[currentwaveIndex]);
+        }
+    }
+
+}
+
+[System.Serializable]
+public struct Wave
+{
+    public float spawmTime;
+    public int maxEnemyCount;
+    public GameObject[] enmyPrefabs;
+}
