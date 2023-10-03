370e15a3d93b811aacb85c9ef45270d3814a1c80
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-03 21:45:54 +0900

    [KYG_ADD] Tower scriptableObject 추가

--- Assets/Scripts/Towers/TowerPosition/TowerWeapon.cs
+++ Assets/Scripts/Towers/TowerPosition/TowerWeapon.cs
@@ -10,15 +10,24 @@ public enum WeaponState
 
 public class TowerWeapon : MonoBehaviour
 {
+    [SerializeField] private TowerTemplate towerTemplate; // 타워 정보
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform spawnPoint;
-    [SerializeField] private float attackRate = 0.5f;
-    [SerializeField] private float attackRange = 2.0f;
-    [SerializeField] private int attackDamage = 1;
 
+    /*[SerializeField] private float attackRate = 0.5f;
+    [SerializeField] private float attackRange = 2.0f;
+    [SerializeField] private int attackDamage = 1;*/
+
+    private int level = 0;
     private WeaponState weaponState = WeaponState.SearchTarget;
     private Transform attackTarget = null;
     private EnemySpawner enemySpawner;
+
+    public Sprite TowerSprite => towerTemplate.weapon[level].sprite;
+    public float Damage => towerTemplate.weapon[level].damage;
+    public float Rate => towerTemplate.weapon[level].rate;
+    public float Range => towerTemplate.weapon[level].range;
+    public int Level => level + 1;
 
 	[SerializeField] private SpriteRenderer characterRenderer;
     [SerializeField] private SpriteRenderer projectilePoint;
@@ -61,7 +70,7 @@ public class TowerWeapon : MonoBehaviour
             for (int i = 0; i < enemySpawner.EnemyList.Count; ++i)
             {
                 float distacne = Vector3.Distance(enemySpawner.EnemyList[i].transform.position, transform.position);
-                if (distacne <= attackRange && distacne <= closestDistSqr)
+                if (distacne <= towerTemplate.weapon[level].range && distacne <= closestDistSqr)
                 {
                     closestDistSqr = distacne;
                     attackTarget = enemySpawner.EnemyList[i].transform;
@@ -86,14 +95,14 @@ public class TowerWeapon : MonoBehaviour
             }
 
             float distance = Vector3.Distance(attackTarget.position, transform.position);
-            if (distance > attackRange)
+            if (distance > towerTemplate.weapon[level].range)
             {
                 attackTarget = null;
                 ChangeState(WeaponState.SearchTarget);
                 break;
             }
 
-            yield return new WaitForSeconds(attackRate);
+            yield return new WaitForSeconds(towerTemplate.weapon[level].rate);
 
             SpawnProjectile();
         }
@@ -101,6 +110,6 @@ public class TowerWeapon : MonoBehaviour
     private void SpawnProjectile()
     {
         GameObject clone = Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
-        clone.GetComponent<Projectile>().Setup(attackTarget, attackDamage);
+        clone.GetComponent<Projectile>().Setup(attackTarget, towerTemplate.weapon[level].damage);
     }
 }
