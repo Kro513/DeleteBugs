920aa02f2aa2f846aa6d48fda2134886c95e6062
Dooheee <142399354+Dooheee@users.noreply.github.com>
2023-10-03 22:33:31 +0900

    [LDH_ADD] StartScene
    [LDH_ADD] StartScene

---
+++ Assets/GameSceneMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneMove : MonoBehaviour
+{
+
+    public void GameScnesCtrl()
+    {
+        SceneManager.LoadScene("GameScene");
+        Debug.Log("Game Scenes Go");
+    } 
+}
