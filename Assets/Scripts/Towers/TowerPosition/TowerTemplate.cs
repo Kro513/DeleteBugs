2198cea7521a6c2c499410de2682052d00485074
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-03 22:43:17 +0900

    [KYG_FIXED] Tower scriptableObject 수정

---
+++ Assets/Scripts/Towers/TowerPosition/TowerTemplate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TowerTemplate : ScriptableObject
+{
+    public GameObject towerPrefab;
+    public WeaponState[] weapon;
+
+    [System.Serializable]
+    public struct Weapon
+    {
+        public Sprite sprite;
+        public float damage;
+        public float rate;
+        public float range;
+        public int price;
+    }
+}
