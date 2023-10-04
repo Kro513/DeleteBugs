06ace603eed6f3c40f71779a359a70bf279814b3
[Github <141483527+Kro513@users.noreply.github.com>
2023-10-04 13:37:19 +0900

    [KHJ_FIX] Tower Scripts error

---
+++ Assets/Scripts/Towers/FlySwatter.cs
@@ -0,0 +1,9 @@
+public class FlySwatter : TowerWeapon
+{
+	public void Start()
+	{
+		attackRate = 3;
+		attackRange = 3f;
+		attackDamage = 3;
+	}
+}
---
+++ Assets/Scripts/Towers/Muscipula.cs
@@ -0,0 +1,9 @@
+public class Muscipula : TowerWeapon
+{
+	public void Start()
+	{
+		attackRate = 5f;
+		attackRange = 5;
+		attackDamage = 5;
+	}
+}
